0ded46d65b6fee37dbc15eb011e891dad102c382
Vu Pham Cong Minh <vggff619@gmail.com>
2021-03-19 16:05:04 +0700

    - initialize

---
+++ SimpleShop.DAL/Models/Variation.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleShop.Models
+{
+    public class Variation
+    {
+        [Key]
+        public int variationId { get; set; }
+        public int productId { get; set; }
+        public string variationName { get; set; }
+    }
+}
---
+++ SimpleShop/Controllers/HomeController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+using SimpleShop.Models;
+using System.Diagnostics;
+
+namespace SimpleShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        //private ProductDAL _productServices;
+
+        public HomeController()
+        {
+            //_productServices = productServices;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
