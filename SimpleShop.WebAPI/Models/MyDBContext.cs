260bd52b9e2b3123efb0a4c01819d7d1c03c5427
Tô Phở Không Hàng <vggff619@gmail.com>
2021-03-25 16:23:51 +0700

    Feat : Add Entity Framework

---
+++ SimpleShop.WebAPI/Models/MyDBContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SimpleShop.WebAPI.Models
+{
+    public class MyDBContext : DbContext
+    {
+        public MyDBContext(DbContextOptions options) : base(options: options)
+        {
+
+        }
+
+
+    }
+}
