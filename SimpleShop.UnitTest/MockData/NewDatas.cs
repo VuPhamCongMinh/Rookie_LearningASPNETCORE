e02dda950bc3771d1eddbd3f2d9a11464b367ce1
Tô Phở Không Hàng <vggff619@gmail.com>
2021-04-13 15:34:31 +0700

    Refactor : Mock data added

---
+++ SimpleShop.UnitTest/MockData/NewDatas.cs
@@ -0,0 +1,34 @@
+﻿using SimpleShop.Shared.Models;
+using SimpleShop.Shared.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleShop.UnitTest.MockData
+{
+    public static class NewDatas
+    {
+        public static Product NewProduct () => new Product
+        {
+            productName = "Test Product Request Name",
+            productPrice = 200,
+            productDescription = "Test Product Request Desc"
+        };
+
+        public static ProductPostRequest NewProductPostRequest () => new ProductPostRequest
+        {
+            productName = "Test Product Request Name",
+            productPrice = 200,
+            productDescription = "Test Product Request Desc"
+        };
+
+        public static Category NewCategory () => new Category
+        {
+            categoryName = "Test Category",
+        };
+
+     
+    }
+}
