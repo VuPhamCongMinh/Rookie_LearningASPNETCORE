03f2a39a2b3dac74aacd5998b87a32b568aa7490
Tô Phở Không Hàng <vggff619@gmail.com>
2021-04-13 15:40:41 +0700

    Fix : add mock data before assert

--- SimpleShop.UnitTest/ProductsControllerTest/Tests/Get.cs
+++ SimpleShop.UnitTest/ProductsControllerTest/Tests/Get.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SimpleShop.API.Controllers;
 using SimpleShop.API.Services;
+using SimpleShop.UnitTest.MockData;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -23,6 +24,10 @@ namespace SimpleShop.UnitTest.ProductsControllerTest.Tests
             var dbContext = _fixture.Context;
             var mapper = MapperMock.Get();
             var fileService = FileServiceMock.FilesService();
+            var product1 = NewDatas.NewProduct();
+            
+            dbContext.Products.Add(product1);
+            dbContext.SaveChanges();
 
             var productsService = new ProductService(dbContext, fileService, mapper);
             var productsController = new ProductsController(productsService);
@@ -32,5 +37,32 @@ namespace SimpleShop.UnitTest.ProductsControllerTest.Tests
             Assert.IsType<OkObjectResult>(result.Result);
 
         }
+        [Theory]
+        [InlineData(1)]
+        [InlineData(2)]
+        public async Task GetSingle_Success (int id)
+        {
+            // Arrange
+            var dbContext = _fixture.Context;
+            var mapper = MapperMock.Get();
+            var fileService = FileServiceMock.FilesService();
+            var product1 = NewDatas.NewProduct();
+            var product2 = NewDatas.NewProduct();
+
+            await dbContext.Products.AddRangeAsync(product1,product2);
+            dbContext.SaveChanges();
+
+            var productsService = new ProductService(dbContext, fileService, mapper);
+            var productsController = new ProductsController(productsService);
+            // Act
+            var result = await productsController.GetProduct(id);
+            // Assert
+            Assert.IsType<OkObjectResult>(result.Result);
+
+        }
+
+       
+
+
     }
 }
