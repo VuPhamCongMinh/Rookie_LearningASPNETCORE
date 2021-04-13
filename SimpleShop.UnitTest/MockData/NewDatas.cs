1a27eacb3a9c0f206304c759632884906a6cf205
Tô Phở Không Hàng <vggff619@gmail.com>
2021-04-13 15:56:19 +0700

    Refactor : Update NewProduct & NewProductPostRequest property

--- SimpleShop.UnitTest/MockData/NewDatas.cs
+++ SimpleShop.UnitTest/MockData/NewDatas.cs
@@ -1,4 +1,5 @@
-﻿using SimpleShop.Shared.Models;
+﻿using Microsoft.AspNetCore.Http;
+using SimpleShop.Shared.Models;
 using SimpleShop.Shared.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -10,18 +11,37 @@ namespace SimpleShop.UnitTest.MockData
 {
     public static class NewDatas
     {
-        public static Product NewProduct () => new Product
+        public static Product NewProduct ()
         {
-            productName = "Test Product Request Name",
-            productPrice = 200,
-            productDescription = "Test Product Request Desc"
-        };
+            var id = new Random().Next(0, 20);
+
+
+            return new Product
+            {
+                productId = id,
+                productName = "Test Product Request Name",
+                productPrice = 200,
+                productDescription = "Test Product Request Desc",
+                Images = new List<Image>()
+                {
+                    new Image{imageUrl = "picture1.jpg",productId = id },
+                    new Image{imageUrl = "picture2.jpg",productId = id },
+                    new Image{imageUrl = "picture3.jpg",productId = id },
+                }
+            };
+        }
 
         public static ProductPostRequest NewProductPostRequest () => new ProductPostRequest
         {
             productName = "Test Product Request Name",
             productPrice = 200,
-            productDescription = "Test Product Request Desc"
+            productDescription = "Test Product Request Desc",
+            ImageFiles = new List<IFormFile>()
+            {
+                new FormFile(null,1,1,"picture1.jpg","picture1"),
+                new FormFile(null,2,3,"picture3.jpg","picture2"),
+                new FormFile(null,2,3,"picture2.jpg","picture3"),
+            }
         };
 
         public static Category NewCategory () => new Category
@@ -29,6 +49,6 @@ namespace SimpleShop.UnitTest.MockData
             categoryName = "Test Category",
         };
 
-     
+
     }
 }
