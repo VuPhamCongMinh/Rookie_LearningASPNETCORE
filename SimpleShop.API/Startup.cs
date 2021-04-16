8854a93428c2758f4f82183d61f23dcc2e74680c
Tô Phở Không Hàng <vggff619@gmail.com>
2021-04-16 15:20:50 +0700

    Add Allow Origin to CORS Policy

--- SimpleShop.API/Startup.cs
+++ SimpleShop.API/Startup.cs
@@ -132,7 +132,7 @@ namespace SimpleShop.API
             app.UseIdentityServer();
             app.UseAuthorization();
 
-            app.UseCors(option => { option.AllowAnyHeader().AllowAnyMethod().AllowCredentials().SetIsOriginAllowed(host => true); });
+            app.UseCors(option => { option.WithOrigins("http://localhost:3000/").AllowAnyHeader().AllowAnyMethod().AllowCredentials().SetIsOriginAllowed(host => true); });
 
             app.UseSwagger();
             app.UseSwaggerUI(c =>
