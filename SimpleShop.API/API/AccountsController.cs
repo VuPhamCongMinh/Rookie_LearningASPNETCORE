d0c6a204c7862d9a42ebb217535a6bfb85aa6c25
Tô Phở Không Hàng <vggff619@gmail.com>
2021-04-22 19:36:48 +0700

    Feat : Add GetAllUsers method

---
+++ SimpleShop.API/API/AccountsController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SimpleShop.Shared.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace SimpleShop.API.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AccountsController : ControllerBase
+    {
+        private readonly MyDBContext context;
+
+        public AccountsController (MyDBContext context)
+        {
+            this.context = context;
+        }
+        // GET: api/<AccountsController>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<IdentityUser>>> GetUsers ()
+        {
+            var users = await context.Users.ToListAsync();
+            return Ok(users);
+        }
+
+        // GET api/<AccountsController>/5
+        [HttpGet("{id}")]
+        public string Get (int id)
+        {
+            return "value";
+        }
+
+        // POST api/<AccountsController>
+        [HttpPost]
+        public void Post ([FromBody] string value)
+        {
+        }
+
+        // PUT api/<AccountsController>/5
+        [HttpPut("{id}")]
+        public void Put (int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<AccountsController>/5
+        [HttpDelete("{id}")]
+        public void Delete (int id)
+        {
+        }
+    }
+}
