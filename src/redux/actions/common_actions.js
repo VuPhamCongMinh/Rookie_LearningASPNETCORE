4f7c0057db7782d601dfd4fa6e048349a2345386
Tô Phở Không Hàng <vggff619@gmail.com>
2021-04-25 16:40:34 +0700

    Thêm api Users vào hàm gọi api

--- src/redux/actions/common_actions.js
+++ src/redux/actions/common_actions.js
@@ -1,15 +1,18 @@
 import axios from "axios";
+import { GetUsers } from "../../api/account_api";
 import { GetCategories } from "../../api/category_api";
 import { GetProducts } from "../../api/product_api";
 import { setCategories } from "./category_actions";
 import { setProducts } from "./product_actions";
+import { setUsers } from "../slices/account_slice";
 
 export const fetchInitialDatas = () => {
   return (dispatch) => {
-    axios.all([GetCategories(), GetProducts()]).then(
-      axios.spread((cateRes, prodRes) => {
+    axios.all([GetCategories(), GetProducts(), GetUsers()]).then(
+      axios.spread((cateRes, prodRes, userRes) => {
         dispatch(setCategories(cateRes.data));
         dispatch(setProducts(prodRes.data));
+        dispatch(setUsers(userRes.data));
       })
     );
   };
